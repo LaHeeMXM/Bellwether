2e1959aa1eaf448015617fa6b2ca854e8389d36e
Kayn <hj479491160@gmail.com>
2025-10-06 06:39:44 +0100

    buff补全

--- Assets/Scripts/Buff.cs
+++ Assets/Scripts/Buff.cs
@@ -35,13 +35,33 @@ public static class Buff
 {
     //可以使用字符串来配置Buff函数
     public static Dictionary<string, BuffFunction> buffFunctionDict = new Dictionary<string, BuffFunction>{
+
+        {"HP",AddHealthSelf},
         {"HPF",AddHealthForward},
         {"HPB",AddHealthBackward},
+        
+        {"ATK",AddAttackSelf},
         {"ATKF",AddAttackForward},
         {"ATKB",AddAttackBackward},
-        //{"DEFF",AddDefenceForward},
-        //{"DEFB",AddDefenceBackward},
+       
+        {"DEF",AddDefenceSelf},
+        {"DEFF",AddDefenceForward},
+        {"DEFB",AddDefenceBackward},
 
+        // --- 全体 (All) ---
+        {"HPA", AddHealthAll},
+        {"ATKA", AddAttackAll},
+        {"DEFA", AddDefenseAll},
+
+        // --- 相邻 (Near) ---
+        {"HPN", AddHealthNear},
+        {"ATKN", AddAttackNear},
+        {"DEFN", AddDefenseNear},
+        
+        // --- 复合自身 (Compound Self) ---
+        {"HPATK", AddHealthAttackSelf},
+        {"HPDEF", AddHealthDefenseSelf},
+        {"ATKDEF", AddAttackDefenseSelf}
     };
 
 
@@ -54,37 +74,178 @@ public static class Buff
 
 
     #region Buff Functions
-    public static UnitAttribute AddHealthForward(BuffInfo info)
+    public static UnitAttribute AddHealthSelf(BuffInfo info)
     {
         UnitAttribute res = new UnitAttribute(0, 0, 0);
-        if (info.index > info.targetIndex) res += new UnitAttribute(0, info.level * info.param.a + info.param.b, 0);
-        return res;
-    }
-    public static UnitAttribute AddHealthBackward(BuffInfo info)
-    {
-        UnitAttribute res = new UnitAttribute(0, 0, 0);
-        if (info.index < info.targetIndex) 
+        if (info.index == info.targetIndex) 
             res += new UnitAttribute(0, info.level * info.param.a + info.param.b, 0);
         return res;
     }
-    public static UnitAttribute AddAttackForward(BuffInfo info)
+
+    public static UnitAttribute AddHealthForward(BuffInfo info)
     {
         UnitAttribute res = new UnitAttribute(0, 0, 0);
-        if(info == null) Debug.Log("Info is null");
-        if (info.param == null) Debug.Log("Info.Param is null");
-        if (info.index > info.targetIndex)
+        if (info.index >= info.targetIndex) 
+            res += new UnitAttribute(0, info.level * info.param.a + info.param.b, 0);
+        return res;
+    }
+
+    public static UnitAttribute AddHealthBackward(BuffInfo info)
+    {
+        UnitAttribute res = new UnitAttribute(0, 0, 0);
+        if (info.index <= info.targetIndex) 
+            res += new UnitAttribute(0, info.level * info.param.a + info.param.b, 0);
+        return res;
+    }
+
+    public static UnitAttribute AddAttackSelf(BuffInfo info) 
+    {
+        UnitAttribute res = new UnitAttribute(0, 0, 0);
+        if (info.index == info.targetIndex)
             res += new UnitAttribute(info.level * info.param.a + info.param.b, 0, 0);
         return res;
     }
+
+    public static UnitAttribute AddAttackForward(BuffInfo info)
+    {
+        UnitAttribute res = new UnitAttribute(0, 0, 0);
+        if (info.index >= info.targetIndex)
+            res += new UnitAttribute(info.level * info.param.a + info.param.b, 0, 0);
+        return res;
+    }
+
     public static UnitAttribute AddAttackBackward(BuffInfo info)
     {
         UnitAttribute res = new UnitAttribute(0, 0, 0);
-        if (info.index < info.targetIndex)
-            res += new UnitAttribute(info.level * info.param.a + info.param.b, 0,0);
+        if (info.index <= info.targetIndex)
+            res += new UnitAttribute(info.level * info.param.a + info.param.b, 0, 0);
         return res;
     }
+
+    public static UnitAttribute AddDefenceSelf(BuffInfo info)
+    {
+        UnitAttribute res = new UnitAttribute(0, 0, 0);
+        if (info.index == info.targetIndex)
+            res += new UnitAttribute(0, 0, info.level * info.param.a + info.param.b);
+        return res;
+    }
+
+    public static UnitAttribute AddDefenceForward(BuffInfo info)
+    {
+        UnitAttribute res = new UnitAttribute(0, 0, 0);
+        if (info.index >= info.targetIndex)
+            res += new UnitAttribute(0, 0, info.level * info.param.a + info.param.b);
+        return res;
+    }
+
+    public static UnitAttribute AddDefenceBackward(BuffInfo info)
+    {
+        UnitAttribute res = new UnitAttribute(0, 0, 0);
+        if (info.index <= info.targetIndex)
+            res += new UnitAttribute(0, 0, info.level * info.param.a + info.param.b);
+        return res;
+    }
+
     #endregion
 
+    #region All Buffs (为全体提供加成)
+
+    public static UnitAttribute AddHealthAll(BuffInfo info)
+    {
+        // 全体Buff，排除对自己生效，避免与自身Buff重复计算
+        if (info.index != info.targetIndex)
+        {
+            return new UnitAttribute(0, info.level * info.param.a + info.param.b, 0);
+        }
+        return new UnitAttribute(0, 0, 0);
+    }
+
+    public static UnitAttribute AddAttackAll(BuffInfo info)
+    {
+        if (info.index != info.targetIndex)
+        {
+            return new UnitAttribute(info.level * info.param.a + info.param.b, 0, 0);
+        }
+        return new UnitAttribute(0, 0, 0);
+    }
+
+    public static UnitAttribute AddDefenseAll(BuffInfo info)
+    {
+        if (info.index != info.targetIndex)
+        {
+            return new UnitAttribute(0, 0, info.level * info.param.a + info.param.b);
+        }
+        return new UnitAttribute(0, 0, 0);
+    }
+
+    #endregion
+
+    #region Near Buffs (为前后相邻提供加成)
+
+    public static UnitAttribute AddHealthNear(BuffInfo info)
+    {
+        // Math.Abs计算索引差的绝对值。如果等于1，说明是紧邻的
+        if (Math.Abs(info.index - info.targetIndex) == 1)
+        {
+            return new UnitAttribute(0, info.level * info.param.a + info.param.b, 0);
+        }
+        return new UnitAttribute(0, 0, 0);
+    }
+
+    public static UnitAttribute AddAttackNear(BuffInfo info)
+    {
+        if (Math.Abs(info.index - info.targetIndex) == 1)
+        {
+            return new UnitAttribute(info.level * info.param.a + info.param.b, 0, 0);
+        }
+        return new UnitAttribute(0, 0, 0);
+    }
+
+    public static UnitAttribute AddDefenseNear(BuffInfo info)
+    {
+        if (Math.Abs(info.index - info.targetIndex) == 1)
+        {
+            return new UnitAttribute(0, 0, info.level * info.param.a + info.param.b);
+        }
+        return new UnitAttribute(0, 0, 0);
+    }
+
+    #endregion
+
+    #region Compound Self Buffs (为自己提供复合加成)
+
+    public static UnitAttribute AddHealthAttackSelf(BuffInfo info)
+    {
+        // 同样只对自己生效
+        if (info.index == info.targetIndex)
+        {
+            // 参数a用于HP, 参数b用于ATK (这是一个约定)
+            return new UnitAttribute(info.level * info.param.b, info.level * info.param.a, 0);
+        }
+        return new UnitAttribute(0, 0, 0);
+    }
+
+    public static UnitAttribute AddHealthDefenseSelf(BuffInfo info)
+    {
+        if (info.index == info.targetIndex)
+        {
+            // 参数a用于HP, 参数b用于DEF
+            return new UnitAttribute(0, info.level * info.param.a, info.level * info.param.b);
+        }
+        return new UnitAttribute(0, 0, 0);
+    }
+
+    public static UnitAttribute AddAttackDefenseSelf(BuffInfo info)
+    {
+        if (info.index == info.targetIndex)
+        {
+            // 参数a用于ATK, 参数b用于DEF
+            return new UnitAttribute(info.level * info.param.a, 0, info.level * info.param.b);
+        }
+        return new UnitAttribute(0, 0, 0);
+    }
+
+    #endregion
 
 
 }
